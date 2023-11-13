b9ad7637004c04057d2c6d020dac7fc01f079213
SalchukArtem <archik.sv@gmail.com>
2023-11-13 19:28:07 +0200

    27.09.2023 Task 2 added to main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 print("Task Date: 27.09.2023")
-print("Task X:\n")
+print("Task 2:\n")
 
 """
-
+Реалізуйте рекурсивну функцію, яка знаходить суму
+чисел в заданому діапазоні.
 """
+
+def recursive_sum(start, end):
+    if start > end:
+        return 0
+    else:
+        return start + recursive_sum(start + 1, end)
 
 # main
+start_num = 1
+end_num = 5
+result = recursive_sum(start_num, end_num)
+print("Sum of numbers from", start_num, "to", end_num, "is", result)
