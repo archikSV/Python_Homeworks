8c7b7614088e66818b3d69a9b5a95f52e52d9e44
SalchukArtem <archik.sv@gmail.com>
2023-11-13 16:55:03 +0200

    18.10.2023 Task 3 added to main.py

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 print("Task Date: 18.10.2023")
-print("Task X:\n")
+print("Task 3:\n")
 
 """
-
+Маємо текстовий файл. Видаліть з нього останній рядок.
+Результат запишіть до іншого файлу.
 """
+
+def remove_last_line(input_file, output_file):
+    with open(input_file, 'r') as file:
+        lines = file.readlines()
+
+    # Remove last line
+    lines = lines[:-1]
+
+    # Write remaining lines to output file
+    with open(output_file, 'w') as file:
+        file.writelines(lines)
 
 # main
+input_file = input("Enter the path to the input file: ")
+output_file = input("Enter the path to the output file: ")
+
+remove_last_line(input_file, output_file)
