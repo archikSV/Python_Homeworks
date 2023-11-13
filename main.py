319835753f284955802d26f366c0ba9c1071febf
SalchukArtem <archik.sv@gmail.com>
2023-11-13 19:10:51 +0200

    9.10.2023 Task 3 added to main.py

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 print("Task Date: 9.10.2023")
-print("Task X:\n")
+print("Task 3:\n")
 
 """
 Маємо три кортежі цілих чисел. Напишіть функцію,
-яка знаходить елементи, які є у всіх кортежах.
+яка знаходить елементи, які є в кожному з кортежів і
+знаходяться в кожному з них на тій самій позиції.
 """
 
-def find_common_elements(tuple1, tuple2, tuple3):
-    common_elements = set(tuple1) & set(tuple2) & set(tuple3)
-    return list(common_elements)
+def find_common_elements_at_same_position(tuple1, tuple2, tuple3):
+    common_elements = []
+    for i in range(len(tuple1)):
+        if tuple1[i] == tuple2[i] == tuple3[i]:
+            common_elements.append(tuple1[i])
+    return common_elements
 
 # main
 tuple1 = (1, 2, 3, 4)
 tuple2 = (3, 4, 5, 6)
 tuple3 = (4, 5, 6, 7)
 
-common_elements = find_common_elements(tuple1, tuple2, tuple3)
+common_elements = find_common_elements_at_same_position(tuple1, tuple2, tuple3)
 print(common_elements)
