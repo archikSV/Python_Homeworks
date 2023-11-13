02452449dbe89f627518bb189cb28ac5c0310af8
SalchukArtem <archik.sv@gmail.com>
2023-11-13 19:18:36 +0200

    4.10.2023 Task 2 added to main.py

--- main.py
+++ main.py
@@ -1,52 +1,51 @@
 print("Task Date: 4.10.2023")
-print("Task 1:\n")
+print("Task 2:\n")
 
 """
-Напишіть програму «Довідник». Створіть два списки
-цілих. Один список зберігає ідентифікаційні коди, другий —
-телефонні номери. Реалізуйте меню для користувача:
-■ відсортувати за ідентифікаційними кодами;
-■ відсортувати за номерами телефонів;
-■ вивести список користувачів з кодами та телефонами;
+Напишіть програму «Книги». Створіть два списки з даними. Один список зберігає назви книг, другий — роки випуску.
+Реалізуйте меню для користувача:
+■ відсортувати за назвою книг;
+■ відсортувати за рокам випуску;
+■ вивести список книг з назвами та роками випуску;
 ■ вихід.
 """
 
-def sort_by_id_codes(id_codes, phone_numbers):
-    sorted_data = sorted(zip(id_codes, phone_numbers))
-    sorted_id_codes, sorted_phone_numbers = zip(*sorted_data)
-    return sorted_id_codes, sorted_phone_numbers
+def sort_by_book_title(book_titles, release_years):
+    sorted_data = sorted(zip(book_titles, release_years))
+    sorted_book_titles, sorted_release_years = zip(*sorted_data)
+    return sorted_book_titles, sorted_release_years
 
-def sort_by_phone_numbers(id_codes, phone_numbers):
-    sorted_data = sorted(zip(phone_numbers, id_codes))
-    sorted_phone_numbers, sorted_id_codes = zip(*sorted_data)
-    return sorted_phone_numbers, sorted_id_codes
+def sort_by_release_year(book_titles, release_years):
+    sorted_data = sorted(zip(release_years, book_titles))
+    sorted_release_years, sorted_book_titles = zip(*sorted_data)
+    return sorted_release_years, sorted_book_titles
 
-def print_directory(id_codes, phone_numbers):
-    for id_code, phone_number in zip(id_codes, phone_numbers):
-        print(f"ID: {id_code} \t Phone: {phone_number}")
+def print_books(book_titles, release_years):
+    for book_title, release_year in zip(book_titles, release_years):
+        print(f"Book: {book_title} \t Release Year: {release_year}")
 
 # Sample data
-id_codes = [1, 3, 2, 4]
-phone_numbers = [555555, 222222, 444444, 111111]
+book_titles = ["Book A", "Book C", "Book B", "Book D"]
+release_years = [2005, 2010, 2000, 2015]
 
 # Main program loop
 while True:
-    print("\nMenu:")
-    print("1. Sort by ID Codes")
-    print("2. Sort by Phone Numbers")
-    print("3. Print Directory")
+    print("Menu:")
+    print("1. Sort by Book Title")
+    print("2. Sort by Release Year")
+    print("3. Print Books")
     print("4. Exit")
 
     choice = input("Choose an option: ")
 
     if choice == "1":
-        sorted_id_codes, sorted_phone_numbers = sort_by_id_codes(id_codes, phone_numbers)
-        print_directory(sorted_id_codes, sorted_phone_numbers)
+        sorted_book_titles, sorted_release_years = sort_by_book_title(book_titles, release_years)
+        print_books(sorted_book_titles, sorted_release_years)
     elif choice == "2":
-        sorted_phone_numbers, sorted_id_codes = sort_by_phone_numbers(id_codes, phone_numbers)
-        print_directory(sorted_id_codes, sorted_phone_numbers)
+        sorted_release_years, sorted_book_titles = sort_by_release_year(book_titles, release_years)
+        print_books(sorted_book_titles, sorted_release_years)
     elif choice == "3":
-        print_directory(id_codes, phone_numbers)
+        print_books(book_titles, release_years)
     elif choice == "4":
         break
     else:
