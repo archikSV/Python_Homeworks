8bb51ae40e80e37e08a6c2e630fa970d4aab50b3
SalchukArtem <archik.sv@gmail.com>
2023-11-13 16:37:51 +0200

    11.10.2023 Task 2 added to main.py

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
 print("Task Date: 11.10.2023")
-print("Task X:\n")
+print("Task 2:\n")
 
 """
-
+Створіть програму «Англо-французький словник». Збережіть слово англійською та його переклад на французьку.
+Реалізуйте можливість додавати, видаляти, знаходити та
+змінювати дані. Використовуйте словник для збереження
+інформації.
 """
+
+dictionary = {}
+
+def add_word(word, translation):
+    dictionary[word] = translation
+
+def delete_word(word):
+    if word in dictionary:
+        del dictionary[word]
+    else:
+        print(f"The word '{word}' is not in the dictionary.")
+
+def search_word(word):
+    if word in dictionary:
+        return dictionary[word]
+    else:
+        return f"The word '{word}' is not in the dictionary."
+
+def update_word(word, translation):
+    if word in dictionary:
+        dictionary[word] = translation
+    else:
+        print(f"The word '{word}' is not in the dictionary.")
 
 # main
+add_word("apple", "pomme")
+add_word("cat", "chat")
+add_word("dog", "chien")
+
+print(search_word("apple"))  # pomme
+
+update_word("apple", "pomme fruit")
+print(search_word("apple"))  # pomme fruit
+
+delete_word("cat")
+print(search_word("cat"))  # The word 'cat' is not in the dictionary.
