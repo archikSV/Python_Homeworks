4e52504ed5479c416080399930f5709f0a4fd48a
SalchukArtem <archik.sv@gmail.com>
2023-11-13 19:13:14 +0200

    9.10.2023 Task 4 added to main.py

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 print("Task Date: 9.10.2023")
-print("Task X:\n")
+print("Task 4:\n")
 
 """
-Маємо три кортежі цілих чисел. Напишіть функцію,
-яка знаходить елементи, які є у всіх кортежах.
+Створіть множину english_vowels з голосними буквами
+англійського алфавіту та множину spanish_vowels з
+голосними буквами іспанського алфавіту. Знайдіть голосні,
+які присутні і в англійській, і в іспанській мовах.
 """
 
-def find_common_elements(tuple1, tuple2, tuple3):
-    common_elements = set(tuple1) & set(tuple2) & set(tuple3)
-    return list(common_elements)
+english_vowels = {'a', 'e', 'i', 'o', 'u'}
+spanish_vowels = {'a', 'e', 'i', 'o', 'u'}
 
-# main
-tuple1 = (1, 2, 3, 4)
-tuple2 = (3, 4, 5, 6)
-tuple3 = (4, 5, 6, 7)
-
-common_elements = find_common_elements(tuple1, tuple2, tuple3)
-print(common_elements)
+common_vowels = english_vowels.intersection(spanish_vowels)
+print(common_vowels)
