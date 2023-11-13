d8e8dae54034b4dd24de56a45402fccf2c6a1d24
SalchukArtem <archik.sv@gmail.com>
2023-11-13 19:09:24 +0200

    9.10.2023 Task 2 added to main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 print("Task Date: 9.10.2023")
-print("Task X:\n")
+print("Task 2:\n")
 
 """
 Маємо три кортежі цілих чисел. Напишіть функцію,
-яка знаходить елементи, які є у всіх кортежах.
+яка знаходитьелементи, які унікальні для кожного списку.
 """
 
-def find_common_elements(tuple1, tuple2, tuple3):
-    common_elements = set(tuple1) & set(tuple2) & set(tuple3)
-    return list(common_elements)
+def find_unique_elements(tuple1, tuple2, tuple3):
+    unique_elements = set(tuple1) ^ set(tuple2) ^ set(tuple3)
+    return list(unique_elements)
 
 # main
 tuple1 = (1, 2, 3, 4)
 tuple2 = (3, 4, 5, 6)
 tuple3 = (4, 5, 6, 7)
 
-common_elements = find_common_elements(tuple1, tuple2, tuple3)
-print(common_elements)
+unique_elements = find_unique_elements(tuple1, tuple2, tuple3)
+print(unique_elements)
